a6db586a7dc38583d2f6f6dc1333504765b6bb47
Makeavoy <auxnon@gmail.com>
2023-02-04 00:36:03 -0500

    feat: initial setup from boilerplate

---
+++ src/lib/Main.svelte
@@ -0,0 +1,14 @@
+<script>
+const test=5
+</script>
+<h1>Petrichor</h1>
+<section>Test</section>
+
+
+<!-- <style lang="scss"> -->
+<!--   section { -->
+<!--     background: var(--color-main); -->
+<!--     border-radius: 3px; -->
+<!--     height: 100%; -->
+<!--   } -->
+<!-- </style> -->
